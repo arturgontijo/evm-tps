b5fb046daa188aa0dfb229f3130fc879185b8f67
Artur Gontijo <arturgontijo@gmail.com>
2023-05-25 14:10:32 -0300

    [sendTransaction] Adding gasPrice.

--- scripts/tps.ts
+++ scripts/tps.ts
@@ -146,6 +146,9 @@ const sendRawTransactions = async (
 ) => {
   console.log(`\n[  TPS ] Sending ${config.transactions} Axios-RAW transfer() transactions...`);
 
+  let gasLimit = ethers.BigNumber.from(config.gasLimit);
+  let gasPrice = await ethers.provider.getGasPrice();
+
   let mapping: Mapping[] = [];
   for (let idx in senders) {
     let sender = senders[idx];
@@ -163,8 +166,8 @@ const sendRawTransactions = async (
 
     unsigned = {
       ...unsigned,
-      gasLimit: ethers.BigNumber.from(config.gasLimit),
-      gasPrice: await ethers.provider.getGasPrice(),
+      gasLimit,
+      gasPrice,
       nonce: await sender.getTransactionCount(),
       chainId: config.chainId,
     };
@@ -219,7 +222,7 @@ const sendRawTransactions = async (
 
     } else {
       // @ts-ignore
-      let tx = await mapping[mIdx].token![config.tokenMethod](config.tokenTransferMultiplier, mapping[mIdx].receiver.address, 1, { gasLimit: config.gasLimit });
+      let tx = await mapping[mIdx].token![config.tokenMethod](config.tokenTransferMultiplier, mapping[mIdx].receiver.address, 1, { gasLimit, gasPrice });
       last = tx.hash;
     }
 
